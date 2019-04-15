73d1a86061d1b8c13c29c55fce4beac0fa5908fc
naoufel raouine <naoufel.raouine@gmail.com>
2019-04-15 11:02:04 +0200

    fin demo 4

--- DemoConsole04/Program.cs
+++ DemoConsole04/Program.cs
@@ -12,7 +12,50 @@ namespace DemoConsole04
         static void Main(string[] args)
         {
             //TestInterface();
-            TestIcomparable();
+            //TestIcomparable();
+            //TestGeneriques();
+            TestCollectionsGenerique();
+
+        }
+
+        private static void TestCollectionsGenerique()
+        {
+            //utilisation d'un dictionnaire generique
+            List<string> lstPrenoms = new List<string> { "Agnes", "Boumedene", "Bogdan" };
+            Console.WriteLine(lstPrenoms[1]);
+
+            Dictionary<int, string> dicoPrenoms = new Dictionary<int, string>
+            { { 17, "Joffrey" },{ 12, "Billele" },{ 31,"Salam"} };
+            Console.WriteLine(dicoPrenoms[12]);
+        }
+
+        private static void TestGeneriques()
+        {
+            string strChaineA = "Anette aime la danette";
+            string strChaineB = "Naoufel aime la caramel";
+            string strChaineC = "Bogdan aime les bananes";
+
+            string[] strPrenoms = { strChaineA, strChaineB, strChaineC };
+
+            string[] strPrenomsRenverse = Renverse<string>(strPrenoms);
+
+            foreach (var item in strPrenomsRenverse)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static T[] Renverse<T>(T[] strPrenoms)
+        {
+            //utilisation des types generique
+            T[] retour = new T[strPrenoms.Length];
+            int intCompteur = strPrenoms.Length - 1;
+            foreach (T item in strPrenoms)
+            {
+                retour[intCompteur] = item;
+                intCompteur--;
+            }
+            return retour;
         }
 
         private static void TestIcomparable()
@@ -54,9 +97,9 @@ namespace DemoConsole04
             foreach (IVehicule item in vehParc)
             {
                 item.Rouler(25);
-                Console.WriteLine("Points de controle: " + item.Controler());
-                Console.WriteLine("Nombre des roues: "+item.byNbRoues);
-                Console.WriteLine("------------------------------");
+                //Console.WriteLine("Points de controle: " + item.Controler());
+                //Console.WriteLine("Nombre des roues: "+item.byNbRoues);
+                //Console.WriteLine("------------------------------");
             } 
         }
     }

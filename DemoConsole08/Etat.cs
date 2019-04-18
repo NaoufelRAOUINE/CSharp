13fb5a70667dc8dca8810ae44d436faf7079ab72
naoufel raouine <naoufel.raouine@gmail.com>
2019-04-18 09:11:26 +0200

    WPF09

---
+++ DemoConsole08/Etat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole08
+{
+    class Etat
+    {
+        public int id { get; set; }
+        public string country { get; set; }
+        public string name { get; set; }
+        public string abbr { get; set; }
+        public string area { get; set; }
+        public string largest_City { get; set; }
+        public string capital { get; set; }
+
+    }
+}
---
+++ DemoConsole08/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient clientWS = new HttpClient();
+            clientWS.BaseAddress = new Uri("http://services.groupkt.com/state/get/IND/all");
+            clientWS.DefaultRequestHeaders.Clear();
+            clientWS.DefaultRequestHeaders.Accept
+                .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            List<Etat> etats = null;
+            HttpResponseMessage response = clientWS.GetAsync("http://services.groupkt.com/state/get/IND/all").Result;
+            etats = response.Content.ReadAsAsync((typeof<List<Etat>>).Result;
+            foreach (var etat in etats)
+            {
+                Console.WriteLine(etat);
+            }
+
+        }
+    }
+}

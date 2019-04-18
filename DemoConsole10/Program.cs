20a86787bd84b0f487cd415f87534684eeb75934
naoufel raouine <naoufel.raouine@gmail.com>
2019-04-18 14:09:49 +0200

    task,async,await

---
+++ DemoConsole10/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoConsole10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DemoTask();
+            //DemoAppelSyncsurMethodAsync();
+            DemoAppelAsynchrone();
+        }
+
+        private async static void DemoAppelAsynchrone()
+        {
+            Console.WriteLine("Appel asynchrone lancé");
+            Traitement tr2 = new Traitement();
+            long result = await tr2.ComputeAsync();
+            Console.WriteLine("fin de l'appel async");
+        }
+
+        private static void DemoAppelSyncsurMethodAsync()
+        {
+            Traitement tr2 = new Traitement();
+            long result=tr2.ComputeAsync().Result;
+            Console.WriteLine("La thread principale vous parle");
+            Console.WriteLine(result); 
+        }
+
+        private static void DemoTask()
+        {
+            Console.WriteLine("Debut de traitement");
+            CancellationTokenSource tokenSourceAnnulation = new CancellationTokenSource();
+            CancellationToken jetonAnnuler = tokenSourceAnnulation.Token;
+            Task<long> tacheCalcul = new Task<long>(
+                () => CalculAsync(jetonAnnuler)
+
+
+                );
+
+            tacheCalcul.Start();
+
+
+            do
+            {
+                if (tacheCalcul.IsCompleted)
+                {
+                    Console.WriteLine(tacheCalcul.Result);
+                }
+            } while (Console.ReadLine() != "");
+            tokenSourceAnnulation.Cancel();
+            //tacheCalcul.Wait();
+            //Console.WriteLine(tacheCalcul.Result);
+            Console.WriteLine("Fin de traitement");
+            Console.ReadLine();
+        }
+
+        static long CalculAsync(CancellationToken jeton)
+        {
+            Traitement tr1 = new Traitement();
+            return tr1.Calculer(jeton);
+        }
+    }
+}
---
+++ DemoConsole10/Traitement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoConsole10
+{
+    class Traitement
+    {
+        public long Calculer(CancellationToken j)
+        {
+            Stopwatch chrono = new Stopwatch();
+            chrono.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                if (j.IsCancellationRequested)
+                {
+                    return -1;
+                }
+                     Thread.Sleep(1000);
+                     Console.WriteLine("Nous avons réalisé {0} secondes de traitement",i+1);
+                
+               
+            }
+            chrono.Stop();
+            return chrono.ElapsedMilliseconds;
+        }
+
+
+        public async Task<long> ComputeAsync()
+        {
+            Console.WriteLine("La méthodeComputeAsync a été appelée");
+
+            Task<long> t = new Task<long>(
+                () =>
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                    return 123;
+                }
+                );
+            t.Start();
+            return t.Result;
+        }
+    }
+}

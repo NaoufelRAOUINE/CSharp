ad68a54dd587619b19d8453daf3c91c6386bff54
naoufel raouine <naoufel.raouine@gmail.com>
2019-04-19 09:22:12 +0200

    collect garbage

---
+++ DemoConsole11/Lecteur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole11
+{
+    class Lecteur:IDisposable
+    {
+        public  FileStream flux{ get; set; }
+        public Lecteur()
+        {
+            flux = File.Open(@"C:\travail\data.txt", FileMode.Open, FileAccess.Read, FileShare.None);
+
+        }
+        public string Lire()
+        {
+            return "Lecture en cours";
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        ~Lecteur()
+        {
+            flux.Close();
+            flux = null;
+            Console.WriteLine("destruction");
+        }
+    }
+}
---
+++ DemoConsole11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SHDocVw;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DemoAutomation();
+            DemoGarbage();
+        }
+
+        private static void DemoGarbage()
+        {
+            Lecteur monLecteur = new Lecteur();
+            Console.WriteLine(monLecteur.Lire());
+            Console.ReadLine();
+            monLecteur = null;
+            Console.WriteLine("Les ressources devraient etre liberes");
+            
+            GC.Collect();
+
+
+        }
+
+        private static void DemoAutomation()
+        {
+            Process.Start("IExplore.exe", "www.google.com");
+            dynamic ie = new InternetExplorer();
+            ie.Visible = true;
+            //ie.FullScreen = true;
+            ie.Navigate("http://www.google.com");
+            Console.Read();
+            
+
+        }
+    }
+}

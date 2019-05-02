0a1dcfeff588a3fb0ac8dcf19d3b007e34dce01e
naoufel raouine <naoufel.raouine@gmail.com>
2019-05-02 12:15:31 +0200

    connexion bdd

---
+++ DemoADO/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoADO
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            DemoADOConnection();
+            //DemoBoissons();
+           
+        }
+
+        private static void DemoBoissons()
+        {
+            
+
+            //Recuperation de la requete
+            string strRequete= "SELECT ProductName, UnitPrice FROM Products p JOIN Categories c ON (p.CategoryID = c.CategoryID) " +
+                                "WHERE c.CategoryName = 'Beverages'";
+            SqlDataReader reader = Requete(strRequete);
+            while (reader.Read())
+            {
+
+                Console.WriteLine(reader.GetSqlString(0) + "-------------" + reader.GetSqlMoney(1).ToString());
+
+            }
+        }
+
+        private static SqlDataReader Requete(string rq)
+        {
+            //Connexion
+            SqlConnection cnx = new SqlConnection();
+            cnx.ConnectionString = @"Data Source=.\sqlexpress;Initial Catalog=Northwind;Integrated Security=True";
+            cnx.Open();
+
+            //Requete aupres de clients
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnx;
+            cmd.CommandText = rq;
+            return cmd.ExecuteReader();
+        }
+
+        private static void DemoADOConnection()
+        {
+          
+            String str = "select * from Customers";
+
+            //Recuperation de la requete
+            SqlDataReader reader = Requete(str);
+            while (reader.Read())
+            {
+
+                Console.WriteLine(reader.GetSqlString(0) + "-------------" + reader.GetSqlString(1));
+
+            }
+        }
+    }
+}
